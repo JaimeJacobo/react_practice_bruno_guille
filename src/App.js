48749ff50e6d5389cc95133be3627cb91643905d
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-11-02 08:13:01 +0900

    Exercises Nov 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
 import { Card } from './components/Card'
-import { PokemonImage } from './components/PokemonImage';
 
 // JSX
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to React!</h1>  
+    <div className="App"> 
       <Card />
-      <PokemonImage />
     </div>
   );
 }
 
 export default App;
-
-// Crear un componente que se llame PokemonImage. Este componente debe renderizar una imagen de un pokemon. Podeis coger la imagen de google. 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react'
+
 export function Card(){
 
-  const name = "Jaime"
+  const [name, setName] = useState('Guille')
+  const [counter, setCounter] = useState(0)
+
+  // name = "Guille" // NUNCA hacerlo asi. Siempre que quieras cambiar tu estado tienes que hacerlo con a función correspondiente a cada estado (setName, setCounter, etc)
+
+  useEffect(()=>{
+    setName('Bruno') // Esta es la forma de cambiar mi estado
+  }, [name])
+
+  useEffect(()=>{
+    console.log(counter)
+  }, [counter])
+
+  const changeCounter =()=>{
+    setCounter(counter + 1)
+  }
+
 
   return(
     <div className="Card">
       <p>Hola {name}</p>
       <p>Bienvenido a React</p>
+      <p>{counter}</p>
+      <button onClick={()=>{changeCounter()}}>Change counter</button>
     </div>
   )
 }
