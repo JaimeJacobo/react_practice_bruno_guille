a29b10ff6cc55aa36fc906d30017902607b28a4b
Jaime Jacobo <jaimejacobordv@gmail.com>
2023-11-13 05:59:57 +0900

    nov 12

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-import { Card } from './components/Card'
-
-// JSX
+import { useState, useEffect } from 'react';
+import { CountryCard } from './components/CountryCard';
 function App() {
+// JavaScript
+const flagsApiUrl = 'https://restcountries.com/v3.1/all?fields=name,flag,languages';
+ 
+const [currentCountries, setCurrentCountries] = useState([])
+
+useEffect(()=>{
+  getFlagsApi()
+}, [])
+
+const getFlagsApi = async () => {
+    const ApiCallResult = await fetch(flagsApiUrl); // llamada a la web de paises
+    const data = await ApiCallResult.json() // Convertir la info a JSON (para que JS pueda leerlo)
+    setCurrentCountries(data)
+}
+
   return (
+    // JSX
     <div className="App"> 
-      <Card />
+      {/* <h1>{currentCountries.length === 0 ? 'Loading...' : currentCountries[0].name.common}</h1> */}
+      <CountryCard />
     </div>
   );
 }
 
 export default App;
+
+
+// https://image.shutterstock.com/image-vector/vector-graphic-no-thumbnail-symbol-260nw-1391095985.jpg
---
+++ src/components/CountryCard.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+export const CountryCard = () => {
+    return(   
+      <div>
+      <img src="https://image.shutterstock.com/image-vector/vector-graphic-no-thumbnail-symbol-260nw-1391095985.jpg" alt="0"></img>
+      <ul>
+        <li>
+        <p>Name</p>
+        <p>language</p>
+        </li>
+      </ul>      
+    </div>
+  )   
+}
